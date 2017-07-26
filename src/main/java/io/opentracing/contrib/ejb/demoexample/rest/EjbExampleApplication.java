b7700ac543eb2b73d3d50f9ebb0bfe8d27c759ad
Kevin Earls <kevin@kevinearls.com>
2017-07-26 10:59:47 +0200

    Almost working, but not writing Spans.

---
+++ src/main/java/io/opentracing/contrib/ejb/demoexample/rest/EjbExampleApplication.java
@@ -0,0 +1,9 @@
+package io.opentracing.contrib.ejb.demoexample.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+
+public class EjbExampleApplication extends Application {
+}
