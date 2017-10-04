34806a250064f6dc9f4d59732b38647b743c9aed
Kevin Earls <kevin@kevinearls.com>
2017-10-04 15:20:38 +0200

    Added maven versions plugin

--- src/main/java/io/opentracing/contrib/ejb/demoexample/rest/EjbExampleApplication.java
+++
@@ -1,9 +0,0 @@
-package io.opentracing.contrib.ejb.demoexample.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/v1")
-
-public class EjbExampleApplication extends Application {
-}
